0c28cba2b7e8661c7f51d79c0e9928635b5aa0c9
Parzival8971 <esh8971@gmail.com>
2022-10-06 17:15:38 +0900

    컴포넌트를 최적화 한다

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useReducer, useRef } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      // {type: "INSERT", todo : {id:1, text:"todo", checked:false}}
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      // {type: "REMOVE", id:1}
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      // {type:"TOGGLE", id:1}
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
 
-  const nextId = useRef(4);
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, []);
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링해 보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false,
+  //   },
+  // ]);
+
+  const nextId = useRef(0);
 
   const onInsert = useCallback((text) => {
     const todo = { id: nextId.current, text, checked: false };
-    setTodos((todos) => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
+    // setTodos((todos) => todos.concat(todo));
     // const todo = [...todos, { id: nextId.current, text, checked: false }];
     // setTodos(todo);
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
+    // setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }, []);
 
   const onToggle = useCallback((id) => {
-    setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
+    dispatch({ type: 'TOGGLE', id });
+    // setTodos((todos) =>
+    //   todos.map((todo) =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo
+    //   )
+    // );
   }, []);
 
   return (
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
+import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className='TodoList'>
-      {todos.map((todo) => (
-        <TodoListItem
-          {...todo}
-          key={todo.id}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
-      ))}
+      {todos.length === 0 ? (
+        <b>오늘의 일정을 작성해보세요!</b>
+      ) : (
+        todos.map((todo) => (
+          <TodoListItem
+            {...todo}
+            key={todo.id}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        ))
+      )}
     </div>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
